11f48c1e88ee761f57a77d69e97a3a9ddc7a0dd8
Pqritz <moritz.zeiler@gmx.de>
2023-05-21 14:36:07 +0200

    Cant stop anywhere now

--- pathFinder/pathFinder/SPAMB.py
+++ pathFinder/pathFinder/SPAMB.py
@@ -10,7 +10,7 @@ class SPAMB:
         self.end = end
         self.must_pass = must_pass
 
-        list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+        list = ['A', 'B' ,'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
         path = self.dijkstra(graph, start, end)
 
         for i in path:
@@ -39,38 +39,12 @@ class SPAMB:
         return nodes, dist
 
     def calc_tent(self, graph, start):
-        tent_dist = self.write_start_tent(graph, start, False)
-        visited = []
-        prio_que = [(start, 0)]
-
-        while prio_que:
-            cur_node = prio_que.pop(prio_que.index((min(prio_que, key=itemgetter(1)))))
-            visited.append(cur_node)
-            cur_node_tent = tent_dist[cur_node[0]]
-            neighbours, distances = self.get_neighbours(graph, cur_node[0])
-
-            for i, neighbour in enumerate(neighbours):
-                dist = distances[i]
-                new_tent = cur_node_tent + dist
-                if new_tent < tent_dist[neighbour]:
-                    tent_dist[neighbour] = new_tent
-
-                    if neighbour not in [item[0] for item in prio_que]:
-                        prio_que.append((neighbour, new_tent))
-                    else:
-                        for i, item in enumerate(prio_que):
-                            if item[0] == neighbour:
-                                prio_que[i] = (neighbour, new_tent)
-                                break
-        return tent_dist
-
-    def dijkstra(self, graph, start, end):
-        #(previous node, weight)
+        # (previous node, weight)
         shortest_paths = {start: (None, 0)}
         cur_node = start
         visited = []
 
-        while cur_node != end:
+        while len(graph) > len(visited):
             visited.append(cur_node)
             weight_to_current = shortest_paths[cur_node][1]
             neighbours, distances = self.get_neighbours(graph, cur_node)
@@ -83,15 +57,25 @@ class SPAMB:
                     current_shortest_weight = shortest_paths[next_node][1]
                     if current_shortest_weight > new_weight:
                         shortest_paths[next_node] = (cur_node, new_weight)
-            next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
+            if all(node in visited for node in neighbours):
+                next_destinations = {node: shortest_paths[node] for node in shortest_paths}
+            else:
+                next_destinations = {node: shortest_paths[node] for node in shortest_paths if node not in visited}
             if not next_destinations:
+                print("here")
                 return "Route Not Possible"
             # next node is the destination with the lowest weight
             cur_node = min(next_destinations, key=lambda k: next_destinations[k][1])
 
             # Work back through destinations in shortest path
+        return shortest_paths
+
+    def dijkstra(self, graph, start, end):
+        shortest_paths = self.calc_tent(graph, start)
+        cur_node = end
         path = []
         while cur_node is not None:
+            print(shortest_paths[cur_node][0])
             path.append(cur_node)
             next_node = shortest_paths[cur_node][0]
             cur_node = next_node
--- pathFinder/pathFinder/main.py
+++ pathFinder/pathFinder/main.py
@@ -3,16 +3,6 @@ import Graph
 
 graph = Graph.Graph(True)
 
-node_list = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-
 graph_data = graph.to_adjacency_list()
+
 finder = pf.SPAMB(graph_data, 1, 4, [0])
-
-x = True
-while x:
-    s = input("What number to convert into Alphabet?")
-    if s == "stop":
-        x = False
-        break
-    print(node_list[int(s)])
-
